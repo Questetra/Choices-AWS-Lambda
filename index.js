e7ee68f8dd9f8b4d0ab36c26616f9875cba65dd2
R_Matsubayashi <matsubayashi@questetra.com>
2021-02-25 10:45:15 +0900

    すべて一つのファイルで処理する形に変更
    環境変数を使用

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const xmlparser = require('xml2json');
 const fs = require('fs');
+const xmlstream = require('node-xml-stream')
 const readline = require("readline");
+
+const file_name = "master/" + process.env.FILE_NAME;
+const file_type = file_name.substr(-3);
 
 exports.handler = async (event) => {
   console.log("Starting ...");
@@ -20,7 +24,15 @@ exports.handler = async (event) => {
   }
 
   try {
-    const responseJson = await streamFileRead('choice.csv', query, parent, values); //ファイル読み込み、完了まで待機
+    let responseJson;
+    if(file_type === "xml"){
+      responseJson = await streamXmlRead(file_name, query, parent, values); //ファイル読み込み、完了まで待機
+    }else if(file_type === "csv" || file_type === "tsv"){
+      responseJson = await streamsvRead(file_name, query, parent, values, file_type); //ファイル読み込み、完了まで待機
+    }else{
+      throw new Error("Invalid Filetype");
+    }
+    
     const responseXml = xmlparser.toXml(responseJson)
     return formatResponse(responseXml);
   } catch (e) {
@@ -54,7 +66,7 @@ function formatError(error) {
   return response;
 }
 
-function streamFileRead(fileName, query, parent, values) {
+function streamXmlRead(fileName, query, parent, values) {
   return new Promise((resolve, reject) => {
     const stream = fs.createReadStream(fileName, {
       encoding: "utf8",         // 文字コード
@@ -65,12 +77,70 @@ function streamFileRead(fileName, query, parent, values) {
         item: []
       }
     };// 読み込んだデータ
+    const parser = new xmlstream(); //XML のパーサー
 
+    parser.on('opentag', (name, attrs) => {
+      // name = 'item'
+      // attrs = { value: '01', display: 'display' }
+      if (name === 'item') {
+        const attrsTmp = {
+          value: attrs.value,
+          display: attrs.display.slice(0, -2)
+        }//display の最後に' /'が含まれてしまうため取り除く
+        //要件に合うもののみ data に入れる
+        if ((query !== "") && (attrsTmp.display.indexOf(query) === -1)) {return;}
+        //query が指定されていて、display に query で指定された文字列を含まないならば弾く
+        if ((parent !== "") && (!attrsTmp.value.startsWith(parent))) {return;}
+        //parent が指定されていて、value が parent で指定された文字列から始まらないならば弾く
+        if ((values.length !== 0) && (values.indexOf(attrsTmp.value) === -1)) {return;}
+        //value が指定されていて、value が values に含まれる文字列と完全一致しないならば弾く
+        data.items.item.push(attrsTmp);
+        //上の条件で弾かれなければ data に入れる
+      }
+    });
+
+    parser.on('finish', () => {
+      // Stream is completed
+      resolve(data);
+    });
+
+    parser.on('error', err => {
+      // Handle a parsing error
+      reject(err);
+    });
+
+    // Stream のエラー処理
+    stream.on("error", (err) => {
+      reject(err);
+    });
+
+    stream.pipe(parser);
+  })
+}
+
+function streamsvRead(fileName, query, parent, values, type) {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(fileName, {
+      encoding: "utf8",         // 文字コード
+      highWaterMark: 1024       // 一度に取得する byte 数
+    });
+    const data = {
+      items: {
+        item: []
+      }
+    };// 読み込んだデータ
+    let split;
+    if(type === "csv"){
+      split = ','
+    }else{
+      //tsv
+      split = '\t'
+    }
     // readlineにStreamを渡す
     const reader = readline.createInterface({ input: stream });
 
     reader.on("line", (line) => {
-      const array = line.split(',');
+      const array = line.split(split);
       const attrsTmp = {
         value: array[0],
         display: array[1]
